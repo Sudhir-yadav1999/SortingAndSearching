c6193818b9d5ac4447cb4d9a2c72298ab2e908b7
Sudhir yadav <54906044+Sudhir-yadav1999@users.noreply.github.com>
2021-05-27 14:03:04 +0530

    Create Insertion_sort.cpp

---
+++ Insertion_sort.cpp
@@ -0,0 +1,45 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+
+void insertionsort(int arr[],int n)
+{
+
+    
+    for(int i=1;i<n;i++)
+    {
+        int temp=arr[i];
+        
+        int j=i-1;
+        while(j>=0 && arr[j]>temp)
+        {
+            
+            arr[j+1]=arr[j];
+            j--;
+        
+        }
+        arr[j+1]=temp;
+    }
+    
+
+
+    for(int j=0;j<n;j++)
+     {
+         cout<<arr[j]<<endl;
+     }
+}
+
+
+int main()
+{
+    //bubble sort
+     int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n=sizeof(arr)/sizeof(arr[0]);
+     insertionsort(arr,n);
+     
+     
+    return 0;
+}
